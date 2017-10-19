00d3c82cd5b453ae8689494d4fbe353284d83893
Montoya Edu <montoya.edu@gmail.com>
2017-10-19 18:51:46 +0200

    Implement half adder.

--- main.c
+++ main.c
@@ -26,6 +26,12 @@ uint8_t nand(uint8_t a, uint8_t b)
     return not(and(a, b));
 }
 
+uint8_t half_add(uint8_t a, uint8_t b, uint8_t* cout)
+{
+    *cout = and(a, b);
+    return xor(a, b);
+}
+
 int main(const int argc, const char** argv)
 {
     fprintf(stdout, "AND GATE\n");
@@ -59,6 +65,14 @@ int main(const int argc, const char** argv)
     for (int a = 0; a <= 1; a++) {
         for (int b = 0; b <= 1; b++) {
             fprintf(stdout, "%x %x = %x\n", a, b, nand(a, b));
+        }
+    }
+    fprintf(stdout, "HALF ADDER\n");
+    fprintf(stdout, "A B RESULT CARRY-OUT\n");
+    for (int a = 0; a <= 1; a++) {
+        for (int b = 0; b <= 1; b++) {
+            uint8_t cout;
+            fprintf(stdout, "%x %x = %x       %x\n", a, b, half_add(a, b, &cout), cout);
         }
     }
     return 0;
