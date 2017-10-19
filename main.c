e8de1be4ee92467836a71dbb67d56c86b005f1e3
Montoya Edu <montoya.edu@gmail.com>
2017-10-19 18:35:15 +0200

    Implement 2 inputs xor gate.

--- main.c
+++ main.c
@@ -11,6 +11,11 @@ uint8_t or(uint8_t a, uint8_t b)
     return a | b;
 }
 
+uint8_t xor(uint8_t a, uint8_t b)
+{
+    return a ^ b;
+}
+
 int main(const int argc, const char** argv)
 {
     fprintf(stdout, "AND GATE\n");
@@ -25,6 +30,13 @@ int main(const int argc, const char** argv)
     for (int a = 0; a <= 1; a++) {
         for (int b = 0; b <= 1; b++) {
             fprintf(stdout, "%x %x = %x\n", a, b, or(a, b));
+        }
+    }
+    fprintf(stdout, "XOR GATE\n");
+    fprintf(stdout, "A B RESULT\n");
+    for (int a = 0; a <= 1; a++) {
+        for (int b = 0; b <= 1; b++) {
+            fprintf(stdout, "%x %x = %x\n", a, b, xor(a, b));
         }
     }
     return 0;
