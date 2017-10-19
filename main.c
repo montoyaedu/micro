97b08f49bb3f8b60ad942ff62ef9fe281aaeb96d
Montoya Edu <montoya.edu@gmail.com>
2017-10-19 18:28:10 +0200

    Implement 2 inputs and gate.

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
+#include <stdint.h>
+#include <stdio.h>
+
+uint8_t and(uint8_t a, uint8_t b)
+{
+    return a & b;
+}
+
 int main(const int argc, const char** argv)
 {
+    for (int a = 0; a <= 1; a++) {
+        for (int b = 0; b <= 1; b++) {
+            fprintf(stdout, "%x & %x = %x\n", a, b, and(a, b));
+        }
+    }
     return 0;
 }
