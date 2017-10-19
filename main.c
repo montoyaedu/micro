2ab0a740e038e3eaca02646d4a1c2866748fd0ae
Montoya Edu <montoya.edu@gmail.com>
2017-10-19 18:31:02 +0200

    Implement 2 inputs or gate.

--- main.c
+++ main.c
@@ -6,11 +6,25 @@ uint8_t and(uint8_t a, uint8_t b)
     return a & b;
 }
 
+uint8_t or(uint8_t a, uint8_t b)
+{
+    return a | b;
+}
+
 int main(const int argc, const char** argv)
 {
+    fprintf(stdout, "AND GATE\n");
+    fprintf(stdout, "A B RESULT\n");
     for (int a = 0; a <= 1; a++) {
         for (int b = 0; b <= 1; b++) {
-            fprintf(stdout, "%x & %x = %x\n", a, b, and(a, b));
+            fprintf(stdout, "%x %x = %x\n", a, b, and(a, b));
+        }
+    }
+    fprintf(stdout, "OR GATE\n");
+    fprintf(stdout, "A B RESULT\n");
+    for (int a = 0; a <= 1; a++) {
+        for (int b = 0; b <= 1; b++) {
+            fprintf(stdout, "%x %x = %x\n", a, b, or(a, b));
         }
     }
     return 0;
