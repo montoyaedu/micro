e6058a6107eb2f2f572cbcc230cfffbda754822f
Montoya Edu <montoya.edu@gmail.com>
2017-10-19 22:40:49 +0200

    Implement adder for n bits. Max number of bits = 8.

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+char* printable(int n, uint8_t* array)
+{
+    char* r = calloc(n + 1, sizeof(char));
+    for (int i = 0, j = n - 1; i < n; i++, j--) {
+        r[j] = array[i] + '0';
+    }
+    return r;
+}
 
 uint8_t and(uint8_t a, uint8_t b)
 {
@@ -39,6 +49,16 @@ uint8_t full_add(uint8_t a, uint8_t b, uint8_t cin, uint8_t* cout)
     uint8_t sum2 = half_add(cin, sum1, &cout2);
     *cout = or(cout1, cout2);
     return sum2;
+}
+
+void n_bit_add(uint8_t bits, uint8_t** result, uint8_t* a, uint8_t* b, uint8_t cin, uint8_t* cout)
+{
+    uint8_t carry_in = cin;
+    for (int i = 0; i < bits; i++) {
+        uint8_t r = full_add(*(a++), *(b++), carry_in, cout);
+        (*result)[i] = r;
+        carry_in = *cout;
+    }
 }
 
 int main(const int argc, const char** argv)
@@ -94,5 +114,14 @@ int main(const int argc, const char** argv)
             }
         }
     }
+    uint8_t cout;
+    uint8_t* r = calloc(4, sizeof(uint8_t));
+    uint8_t a[5] = { 0, 1, 1, 1 };
+    uint8_t b[5] = { 1, 0, 0, 0 };
+    n_bit_add(4, &r, a, b, 0, &cout);
+    fprintf(stdout, "%s + %s = %s, carry = %x\n", printable(4, a), printable(4, b), printable(4, r), cout);
+    a[0] = 1;
+    n_bit_add(4, &r, a, b, 0, &cout);
+    fprintf(stdout, "%s + %s = %s, carry = %x\n", printable(4, a), printable(4, b), printable(4, r), cout);
     return 0;
 }
